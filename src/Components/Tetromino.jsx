3a3d1e9a84ed2adbda629c841cd43a0df725d058
Alex Guo <alexguo247@gmail.com>
2020-06-29 16:09:53 -0400

    Added random tetromino selector function

--- src/Components/Tetromino.jsx
+++ src/Components/Tetromino.jsx
@@ -3,6 +3,9 @@ import React, {useState, useEffect, useCallback} from 'react';
 const Tetromino = (props) => {
     let x, y, color, shape;
 
+    const randomPick = () => {
+      return Math.floor(Math.random() * 7 + 1);
+    }
     //Arrow Key functions
     const movement = useCallback((event) => {
         //Left
